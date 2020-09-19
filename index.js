7fd2e0428d70403247b9bf5a08d87637763cbdda
Mannan Mendiratta <mannanmendiratta99@gmail.com>
2020-09-19 10:17:26 -0500

    Attached Login Page

--- index.js
+++ index.js
@@ -8,9 +8,13 @@ app.use(express.static(__dirname))
     // app.use(express.static(__dirname + "/vendor"))
     // app.use('/static', express.static())
     // app.use('/static', express.static())
-app.get('/', (req, res) => {
+app.get('/dashboard', (req, res) => {
     console.log(__dirname)
     res.sendFile(path.join(__dirname + '/views/index.html'))
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/loginpage/login.html'))
 })
 
 app.listen(process.env.PORT || 5000, () => {
