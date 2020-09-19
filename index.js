c15c97972f760dcce6d3b97713d1a5f496575754
Mannan Mendiratta <mannanmendiratta99@gmail.com>
2020-09-19 10:12:53 -0500

    Sample Dashboard

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 'use strict';
 
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 5000
-
+app.use(express.static(__dirname))
+    // app.use(express.static(__dirname + "/vendor"))
+    // app.use('/static', express.static())
+    // app.use('/static', express.static())
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    console.log(__dirname)
+    res.sendFile(path.join(__dirname + '/views/index.html'))
 })
 
 app.listen(process.env.PORT || 5000, () => {
